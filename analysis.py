7e7cc429a4609491181078d42e04d7b4155b211f
Frank <G00340834@atu.ie>
2023-03-16 13:05:13 +0000

    added basic create summary file function

--- analysis.py
+++ analysis.py
@@ -2,8 +2,21 @@
 # Author: Frank Quinn
 
 import pandas as pd
+import sys
 
 # Data file from : https://archive.ics.uci.edu/ml/datasets/iris
 # Using pandas to load data as a dataframe from iris.data file and add column headings
-data = pd.read_csv("data/iris.csv", sep=",", header = None, names= ["Sepal Length (cm)", "Sepal Width (cm)", "Petal Length (cm)", "Petal Width (cm)", "Flower"])
+data = pd.read_csv("data/iris.csv", sep=",", header = None, names= ["Sepal Length (cm)", "Sepal Width (cm)", "Petal Length (cm)", "Petal Width (cm)", "Species"])
+
+# USing sys.stdout to write to file as it was easier than opening a file and converting a dataframe to string to write
+def create_summary_file():
+    sys.stdout = open("analysis/Summary_analysis.txt", "w")
+    print ("*****************************************************************")
+    print ("Overview of Iris Fisher data:")
+    print ("\n")
+    print(data)
+    print ("*****************************************************************")
+    sys.stdout.close()
+
+create_summary_file()
 
