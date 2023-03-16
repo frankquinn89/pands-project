3cc434216a64593f7c1afd77fea2868354c47e59
Frank <G00340834@atu.ie>
2023-03-16 11:56:23 +0000

    update comment

--- analysis.py
+++ analysis.py
@@ -4,7 +4,6 @@
 import pandas as pd
 
 # Data file from : https://archive.ics.uci.edu/ml/datasets/iris
-# Using pandas to load data as a dataframe from iris.data file and add headings
-# Adding column headings in 
+# Using pandas to load data as a dataframe from iris.data file and add column headings
 data = pd.read_csv("data/iris.csv", sep=",", header = None, names= ["Sepal Length (cm)", "Sepal Width (cm)", "Petal Length (cm)", "Petal Width (cm)", "Flower"])
 
